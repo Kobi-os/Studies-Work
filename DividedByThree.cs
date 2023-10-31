bcd4721e60878f7d276dcd09186c38d3bf55a202
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-10-31 10:32:45 +0100

    Add files via upload

---
+++ DividedByThree.cs
@@ -0,0 +1,28 @@
+﻿namespace DividedByThree
+{
+    internal class DividedByThree
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj Liczbe w przedziale od 1 do 999");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int setki = number / 100;
+            number -= setki * 100;
+            int dziesiatki = number / 10;
+            number -= dziesiatki * 10;
+
+            int jednosci = number;
+
+            int SumOfNumbers = setki + dziesiatki + jednosci;
+
+            if (SumOfNumbers % 3 == 0)
+            {
+                Console.WriteLine("Liczbe jest podzielna przez 3");
+            } else
+            {
+                Console.WriteLine("Liczba nie jest podzielna przez 3");
+            }
+        }
+    }
+}
