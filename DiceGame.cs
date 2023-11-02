875f046117a921d1e187ee87c89c8c0b3e4d4708
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-11-02 11:33:21 +0100

    Add files via upload

---
+++ DiceGame.cs
@@ -0,0 +1,48 @@
+﻿namespace DiceGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int PlayerNumberOne;
+            int EnemyPlayer;
+
+            int PlayerScore = 0;
+            int EnemyScore = 0;
+
+            Random number = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.ReadKey();
+                PlayerNumberOne = number.Next(1, 7);
+                Console.WriteLine("Player Number One numbers " + PlayerNumberOne);
+
+                Console.WriteLine("Next Player");
+                Thread.Sleep(1000);
+                
+
+                EnemyPlayer = number.Next(1, 7);
+                Console.WriteLine("Enemy Player numbers " + EnemyPlayer);
+                
+
+                if (PlayerNumberOne > EnemyPlayer)
+                {
+                    PlayerScore++;
+                    Console.WriteLine("Player got a point" + PlayerScore);
+                } 
+                else if (EnemyPlayer > PlayerScore)
+                {
+                    EnemyScore++;
+                    Console.WriteLine("Enemy got a point" + EnemyScore);
+                }
+                else
+                {
+                    Console.WriteLine("Nobody gets a point the numbers are the same");
+                }
+            }
+        }
+    }
+}
