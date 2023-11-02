3bb10612e789738f792411af93b298d028fc74ae
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-11-02 09:43:17 +0100

    Add files via upload

---
+++ YourAge.cs
@@ -0,0 +1,20 @@
+﻿namespace YourAge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj Swój Wiek");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 18)
+            {
+                Console.WriteLine("Jestes niepełnoletni brak dostępu");
+            } 
+            else
+            {
+                Console.WriteLine("Jestes Pełnoletni możesz wejsc");
+            }
+        }
+    }
+}
