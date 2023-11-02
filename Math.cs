669d24074f02c3cd3cd4f1e480df20e84ea42e73
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-11-02 09:39:35 +0100

    Add files via upload

---
+++ Math.cs
@@ -0,0 +1,11 @@
+﻿namespace math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double wynik = Math.Pow(((((6.2 / 0.31 - 5.0 / 9 * 0.9) * 0.2 + 0.15) / 0.02)), 3);
+            Console.WriteLine("Wynik wyrażenia to: " + wynik);
+        }
+    }
+}
