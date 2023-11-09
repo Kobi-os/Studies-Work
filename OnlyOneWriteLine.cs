55e8c67b6eb27e58bd0004205292cb0ee0c8e48a
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-11-09 13:56:36 +0100

    Add files via upload

---
+++ OnlyOneWriteLine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace OnlyOneWriteLine
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            int a, b, c;
+
+            Console.WriteLine("Podaj a");
+            a = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Podaj b");
+            b = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Podaj c");
+            c = int.Parse(Console.ReadLine());
+
+
+
+            int tmp;
+
+            if (a > b)
+            {
+                tmp = a;
+                a = b;
+                b = tmp;
+            }
+            if (b > c)
+            {
+                tmp = b;    
+                b = c;
+                c = tmp;
+            }
+            if (a > b)
+            {
+                tmp = a;
+                a = b;
+                b = tmp;
+            }
+
+            Console.WriteLine(a + "\t" + b + "\t" + c);
+
+        }
+    }
+}
