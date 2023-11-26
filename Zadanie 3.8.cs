5cf5f61273b45c3f761f6440310e1039fafc6cf2
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-11-26 16:35:42 +0100

    Add files via upload

---
+++ Zadanie 3.8.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Zadanie_3._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x;
+            do
+            {
+                Console.WriteLine("Podaj Liczbe");
+                string userNumber = Console.ReadLine();
+
+                if (double.TryParse(userNumber, out x))
+                {
+                    if (Math.Abs(x) > 2 && Math.Abs(x) < 3)
+                    {
+                        Console.WriteLine("Liczba {0} spelnia warunek 2<|x|<3", x);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Liczba {0} nie spełnia warunku 2<|x|<3", x);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nieprawidłowe dane, Podaj Liczbe ");
+                }
+            } while (true);
+        }
+    }
+}
