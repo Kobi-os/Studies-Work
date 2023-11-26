1e2feb89af3ad81cedeedacfe4b510b39471d78a
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-11-26 15:51:25 +0100

    Add files via upload

---
+++ Zadanie 3.5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Zadanie_3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal lokata = 10000m;
+            decimal oprocentowanie = 0.05m;
+            decimal odsetki = 0;
+            int liczbaMiesiecy = 12;
+
+            for (int i = 0; i < liczbaMiesiecy; i++)
+            {
+                decimal odsetkiMiesieczne = (lokata * oprocentowanie) / 12;
+                odsetki += odsetkiMiesieczne;
+                lokata += odsetkiMiesieczne;
+            }
+
+            Console.WriteLine($"Odestki {odsetki}");
+            Console.WriteLine($"Kwota końcowa {lokata}");
+            Console.WriteLine($"wynik {odsetki:C} ");
+
+        }
+    }
+}
