949eb2d3158dc7569ac794dd641e0722aa75b3a6
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-11-26 15:12:14 +0100

    Add files via upload

---
+++ Zadanie 3.2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zadanie_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbe która będzie pod n");
+            int n = int.Parse(Console.ReadLine());
+            int sumaLiczb = 0;
+            int i = 1;
+
+            while ( i <= n)
+            {
+                sumaLiczb += i;
+                i++;
+            }
+            Console.WriteLine($"Suma liczb wynosi {sumaLiczb}");
+        }
+    }
+}
