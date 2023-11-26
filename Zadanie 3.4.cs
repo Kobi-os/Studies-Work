9a6daaf19cbdc774d39c5992f0fc7a9d751bb36c
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-11-26 15:41:18 +0100

    Add files via upload

---
+++ Zadanie 3.4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Zadanie_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfIterations = 100;
+            double sum = 0;
+
+            for (int n = 0; n <= numberOfIterations; n++)
+            {
+                double term = Math.Pow(-1, n) / (2 * n + 1);
+                sum += term;
+            }
+
+            double result = sum * 4;
+
+            Console.WriteLine($"Przybliżona wartość pi to: " + result);
+        }
+    }
+}
