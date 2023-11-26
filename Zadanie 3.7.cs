5fd144b84b3ddbe0931a35c8195ed86b6ff4b351
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-11-26 16:10:01 +0100

    Add files via upload

---
+++ Zadanie 3.7.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Zadanie_3._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj a ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj b ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int wynik = 1;
+            int i = 0;
+
+            while (i < b)
+            {
+                wynik *= a;
+                i++;
+            }
+
+            Console.WriteLine($"Wynik: {wynik}");
+        }
+    }
+}
