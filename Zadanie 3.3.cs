fd06c209107debc3e88584ee270c2781774ebd0c
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-11-26 15:24:17 +0100

    Add files via upload

---
+++ Zadanie 3.3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Zadanie_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 7;
+            int numberOfNumbers = 0;
+            int sumOfNumbers = 0;
+  
+
+            do
+            {
+                numberOfNumbers++;
+                sumOfNumbers += numberOfNumbers;
+            } while (sumOfNumbers < value); 
+
+            Console.WriteLine($"Suma liczb do dodania {numberOfNumbers}");
+
+        }
+    }
+}
