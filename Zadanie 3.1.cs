72d094507aba7933727fa0da042faa679584742a
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-11-26 14:57:00 +0100

    Add files via upload

---
+++ Zadanie 3.1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Zadanie_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int ilośćLiczb = 0;
+            int sumaLiczb = 0;
+
+            while (sumaLiczb <= 100)
+            {
+                Console.Write("Podaj Liczbę;");
+                int liczba = int.Parse(Console.ReadLine());
+
+                sumaLiczb += liczba;
+                ilośćLiczb += 1;
+
+                Console.WriteLine("Suma liczba wynosi: " + sumaLiczb);
+                Console.WriteLine("Dodaleś : " + ilośćLiczb);
+            }
+        }
+    }
+}
