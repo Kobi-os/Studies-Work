f872cbce2b6a1d6e765025792b8b92f662d2c158
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-12-09 09:44:02 +0100

    Add files via upload

---
+++ Zadanie 4.6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Zadanie_4._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba = 1;
+
+            while (liczba <= 10000)
+            {
+                int sum = 0;
+                int j = 1;
+
+                while( j <= liczba / 2)
+                {
+                    if (liczba % j == 0)
+                    {
+                        sum += j;
+                    }
+                    j++;
+                }
+
+                if (sum == liczba)
+                {
+                    Console.WriteLine(liczba);
+                }
+                liczba++;
+            }
+        }
+    }
+}
