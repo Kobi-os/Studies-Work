a8aa06185f5ddf32848b33594b76c696abc7604b
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-12-09 09:48:52 +0100

    Add files via upload

---
+++ Zadanie 4.7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Zadanie_4._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 11;
+            int height = 5;
+
+            for (int h = 0; h <= height; h++)
+            {
+                for (int w = 0; w <= width; w++)
+                {
+                    if (h == w || h + w == width - 1)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
