e0f5e2d3e0abfa86e0e571193dbfeaf98e475847
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-12-09 09:39:00 +0100

    Add files via upload

---
+++ Zadanie 4.5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Zadanie_4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba = 100;
+
+            do
+            {
+                do
+                {
+                    if (liczba % 2 == 0 || liczba % 3 != 0 || liczba % 7 != 0)
+                    {
+                        liczba++;
+                        continue;
+                    }
+                    Console.WriteLine("Liczba {0} jest nieparzysta i jest podzielna przez 3 i 7", liczba);
+                    liczba++;
+
+                } while (liczba <= 200);
+            } while (liczba <= 200);
+        }
+    }
+}
