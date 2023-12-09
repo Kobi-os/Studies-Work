e2931d786678c17b866f2148eddd67b49f1ffe6b
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-12-09 08:23:45 +0100

    Add files via upload

---
+++ zad4.1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Zadanie_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba_kombinacji = 0;
+
+            for(int i =0; i <= 10; i++)
+            {
+                for(int j = 0; j <= 5; j++)
+                {
+                    for(int k = 0; k <= 2; k++)
+                    {
+                        if(i + j * 2 + k * 5 == 10)
+                        {
+                            Console.WriteLine("kombinacja: {0}, złotówek, {1} dwuzlotówek, {2} pięciozłotówek", i, j, k);
+                            liczba_kombinacji++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
