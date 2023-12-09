ee5e8d158e7d95ad13ec67da1a5d4851123ee1ed
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-12-09 08:55:47 +0100

    Add files via upload

---
+++ Zad4.2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Zad4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w, k;
+            for (w = 1; w <= 10; w++)
+            {
+                for (k = 1; k <= 10; k++)
+                  if (w == k | w + k == 11 | (w >= 4 & w <= 7 & k >= 4 & k <= 7))  
+                         Console.Write(" * ");
+                else
+                        Console.Write("   ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
