ced018c0aa56aee54c6691f61eeeed01548c46f9
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-12-10 15:10:08 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,71 @@
-﻿namespace ConvertAPP
+﻿using System;
+
+namespace Zadanie_5._1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome! Please enter 'c' tu use the currency converter, or anything else to use the temperature converter");
-            Console.ReadLine();
+            Console.WriteLine("Podaj rozmiar tablicy");
+            int rozmiarTablicy = 0;
+            bool userValidInput = false;
+
+            while (!userValidInput)
+            {
+                try
+                {
+                    rozmiarTablicy = int.Parse(Console.ReadLine());
+                    userValidInput = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Nieprawidłowy format danych. Podaj liczbe całkowitą");
+                }
+                catch(OverflowException)
+                {
+                    Console.WriteLine("Podana wartość jest zbyt duża lub zbyt mała.");
+                } 
+                catch (Exception)
+                {
+                    Console.WriteLine("Wystąpił nieoczekiwany błąd");
+                }
+            }
+
+            int[] tablica = new int[rozmiarTablicy];
+
+            for (int i = 0; i < rozmiarTablicy; i++)
+            {
+                Console.Write($"Podaj wartość dla elementu: {i}: ");
+                userValidInput = false;
+
+                while (!userValidInput)
+                {
+                    try
+                    {
+                        tablica[i] = int.Parse(Console.ReadLine());
+                        userValidInput = true;
+                    }
+                    catch(FormatException)
+                    {
+                        Console.WriteLine("Nieprawidłowy format danych. Podaj liczbe całkowitą");
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Podana wartość jest zbyt duża lub zbyt mała.");
+                    }
+                    catch(Exception)
+                    {
+                    Console.WriteLine("Wystąpił nieoczekiwany błąd");
+                    }
+                }
+            }
+
+                Console.WriteLine("Zawartość tablicy:");
+
+                for (int i = 0; i < rozmiarTablicy; i++) {
+                    Console.WriteLine($"Element {i}; {tablica[i]}");
+                }
+            
         }
     }
-} 
+}
