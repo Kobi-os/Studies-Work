aee736f543e38e38cd495536540b6680f2cfd225
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-12-10 15:45:12 +0100

    Add files via upload

---
+++ LowestNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Zadanie_5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] tablica = { 10, 5, 2, 7, 1, 8, 4, 3, 6, 9 };
+            byte najmniejszaLiczba = tablica[0];
+
+            int pozycjaLiczby = 0;
+
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                if (tablica[i] < najmniejszaLiczba)
+                {
+                    najmniejszaLiczba = tablica[i];
+                    pozycjaLiczby = i;
+                }
+            }
+
+            Console.WriteLine("Najmniejszy element tablicy: " + najmniejszaLiczba);
+            Console.WriteLine("Pozycja elementu: " + pozycjaLiczby);
+
+        }
+    }
+}
