8eb929d25631eaf71fc44326c882cded5a92f22a
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2023-12-10 16:19:20 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,35 +1,51 @@
 ﻿using System;
 
-namespace Zadanie_5._2
+namespace Zadanie_5._4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] tablica = new int[30];
+            int[,] tablica = new int[5, 5];
+            Random randomNumber = new Random();
 
-            for (int i = 0; i < tablica.Length; i++)
+            for (int row = 0; row < 5; row++)
             {
-                tablica[i] = i + 1;
+                for (int col = 0; col < 5; col++)
+                {
+                    tablica[row, col] = randomNumber.Next(10);
+                }
             }
 
-            int suma = 0;
+            Console.WriteLine("Tablica 5x5");
 
-            foreach (int num in tablica)
+            for (int row = 0; row < 5; row++)
             {
-                suma += num;
+                for (int col = 0; col < 5; col++)
+                {
+                    Console.Write(tablica[row, col] + " ");
+                }
+                Console.WriteLine();
             }
 
-            double srednia = (double)suma / tablica.Length;
-
-            Console.WriteLine("tablica elementów");
-
-            foreach (int num in tablica)
+            int pierwszaPrzekątna = 0;
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine(num + " ");
+                pierwszaPrzekątna += tablica[i, i];
             }
 
-            Console.WriteLine($"\n Średnia wartosc elementów tablicy: {srednia}");
+            int drugaPrzekątna = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                drugaPrzekątna += tablica[i, 4 - i];
+            }
+
+
+            Console.WriteLine($"Pierwsza przekątna {pierwszaPrzekątna}");
+            Console.WriteLine($"Druga przekątna {drugaPrzekątna}");
+
+
+
         }
     }
 }
